1807ba2b1259b7802fd978dda3945c5cc16c9d03
ender-music <58605060+ender-music@users.noreply.github.com>
2022-11-24 08:51:30 -0700

    initial

---
+++ install.sh
@@ -0,0 +1,11 @@
+pkg install openjdk-17
+
+wget https://github.com/SlimeVR/SlimeVR-Server/releases/download/v0.3.1/slimevr.jar
+
+echo "Installed, run with java -jar slimevr.jar"
+
+echo "Now running, please go to https://slimevr-gui.bscotch.ca/"
+
+echo "Android and Termux useability credit goes to Butterscotch, installer by Bagel."
+
+java -jar slimevr.jar
