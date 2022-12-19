db75c804a798406246d1892ff7c600c144a356bf
lordbagel <58605060+lordbagel42@users.noreply.github.com>
2022-12-18 18:44:53 -0700

    add termux-open-url

--- start.sh
+++ start.sh
@@ -7,9 +7,11 @@ diff slimevr.jar <(curl https://github.com/SlimeVR/SlimeVR-Server/releases/lates
 if [ $? -ne 0 ]
 then
     echo "New version available, updating..."
-    wget -qO slimevr.jar https://github.com/SlimeVR/SlimeVR-Server/releases/latest/download/slimevr.jar>
+    wget -O slimevr.jar https://github.com/SlimeVR/SlimeVR-Server/releases/latest/download/slimevr.jar>
     java -jar slimevr.jar
+    termux-open-url https://slimevr-gui.bscotch.ca/
 else
-    echo "Latest version, not updating..."
+    echo "Latest version installed, not updating..."
     java -jar slimevr.jar
+    termux-open-url https://slimevr-gui.bscotch.ca/
 fi
