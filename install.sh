9eec8921a88cf7831c8bcd78728e6a2d2ed2e955
lordbagel <58605060+lordbagel42@users.noreply.github.com>
2022-12-30 14:26:21 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-pkg install openssl wget openjdk-17
+pkg up -y && pkg install openssl wget openjdk-17
 
 wget https://github.com/SlimeVR/SlimeVR-Server/releases/latest/download/slimevr.jar
 wget -O start.sh https://raw.githubusercontent.com/lordbagel42/slimevr-termux-installer/main/start.sh
