4e2a2c3666d9334387bec6175a905623b01fb195
lordbagel <58605060+lordbagel42@users.noreply.github.com>
2022-12-30 14:16:41 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # compare the local file with the remote file
-diff slimevr.jar <(curl https://github.com/SlimeVR/SlimeVR-Server/releases/latest/download/slimevr.jar>
+diff slimevr.jar <(curl https://github.com/SlimeVR/SlimeVR-Server/releases/latest/download/slimevr.jar)
 
 # check the exit status of the diff command
 if [ $? -ne 0 ]
