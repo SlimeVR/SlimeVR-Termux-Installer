9ecad49d43299265d4745d503763bd4501d2578d
lordbagel <58605060+lordbagel42@users.noreply.github.com>
2023-01-04 20:06:17 -0700

    make things work righter

--- start.sh
+++ start.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # compare the local file with the remote file
-diff slimevr.jar <(curl https://github.com/SlimeVR/SlimeVR-Server/releases/latest/download/slimevr.jar)
+diff slimevr.jar <(curl -s https://github.com/SlimeVR/SlimeVR-Server/releases/latest/download/slimevr.jar)
 
 # check the exit status of the diff command
 if [ $? -ne 0 ]
 then
     echo "New version available, updating..."
-    wget -O slimevr.jar https://github.com/SlimeVR/SlimeVR-Server/releases/latest/download/slimevr.jar
+    wget -N https://github.com/SlimeVR/SlimeVR-Server/releases/latest/download/slimevr.jar
     termux-open-url https://slimevr-gui.bscotch.ca/
     java -jar slimevr.jar
 else
